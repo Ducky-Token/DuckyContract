acb0c9e02b971aa8a1a29c0c226d165b8d47494d
ducky-token <ducky.token@gmail.com>
2021-05-09 17:10:56 +0900

    update ducky token contract

--- contracts/DuckyToken.sol
+++ contracts/DuckyToken.sol
@@ -1,8 +1,19 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DockyToken is ERC20 {
+contract DockyToken is ERC20("DuckyToken", "DUCKY"), Ownable {
+    /**
+     * @dev Burns a specific amount of tokens.
+     * @param value The amount of lowest token units to be burned.
+     */
+    function burn(uint256 value) external {
+      _burn(msg.sender, value);
+    }
 
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
 }
 
