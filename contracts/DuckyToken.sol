7f3a28ebee40d691d0beb90f89dff38b84712fcc
ducky-token <ducky.token@gmail.com>
2021-05-09 17:12:26 +0900

    fix issue

--- contracts/DuckyToken.sol
+++ contracts/DuckyToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DockyToken is ERC20("DuckyToken", "DUCKY"), Ownable {
+contract DuckyToken is ERC20("DuckyToken", "DUCKY"), Ownable {
     /**
      * @dev Burns a specific amount of tokens.
      * @param value The amount of lowest token units to be burned.
